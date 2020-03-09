93d3cc1589584aea4fd09936527da339231f6719
Stephen C. Gilardi <scgilardi@gmail.com>
2020-03-09 00:09:29 -0400

    add lein project.clj for install/deploy
    - shadow-cljs doesn't handle maven concerns

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject sazhet "0.1.0-SNAPSHOT"
+  :description "Utilities for Keechma"
+  :url "http://github.com/scgilardi/sazhet"
+  :license {:name "MIT"
+            :url "https://opensource.org/licenses/MIT"}
+
+  :dependencies [[org.clojure/clojure "1.10.1" :scope "provided"]
+                 [org.clojure/clojurescript "1.10.597" :scope "provided"]])
